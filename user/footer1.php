7f4735ea9fea6f0d342a623f00768ebda558bcf1
Atib Chowdhury <atib.chowdhury@gmail.com>
2014-02-09 16:33:31 +0000

    added css to user css

--- user/footer1.php
+++ user/footer1.php
@@ -1,2 +1,11 @@
-<div class="f01">The Gaylord Tandoori Indian Restaurant <br>
-141 Manchester Road, Isle of Dogs, London E14 3DN. <br>Telephone : 020 7538 9826 and 020 7538 0393 Email : info@gaylordrestaurant.co.uk</div>
+<div class="f01">
+
+<h6> The Gaylord Tandoori Indian Restaurant 
+<br />141 Manchester Road, Isle of Dogs, London E14 3DN. 
+<br />Telephone : 020 7538 9826 and 020 7538 0393
+<br /> Email : info@gaylordrestaurant.co.uk </h6>
+
+	<div id="footer_color">
+		<h6> A Pummello Designed &amp; Developed Application </h6>
+	</div>
+</div>
