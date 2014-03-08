9bfb330cabd537555deb9aef84d267ca69fd4674
Atib Chowdhury <atib.chowdhury@gmail.com>
2014-03-08 14:33:14 +0000

    added stylesheet for customer

--- user/head.php
+++ user/head.php
@@ -4,5 +4,7 @@
 	<title>The Gaylord</title>
 	<link rel="stylesheet" type="text/css" href="../CSS/jquery.mobile-1.4.0.min.css">
 	<link href="../CSS/reset.css" rel="stylesheet" type="text/css">
+	<link href="../CSS/Main.css" rel="stylesheet" type="text/css">
+
 	<script type="text/javascript" src="../Script/jquery.js" ></script>
 </head>
