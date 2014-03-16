f555fa79295b20c051364425aee554d7ba253daf
jobanali <jobanali23@gmail.com>
2014-03-16 16:31:31 +0000

    test script contains character strip by word
    cant remember which page this needs to be added to

--- user/test.php
+++ user/test.php
@@ -1,33 +1,27 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+class Fun {
 
-<body>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=472812626151651";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+    public function limit_text($text, $len) {
+        if (strlen($text) < $len) {
+            return $text;
+        }
+        $text_words = explode(' ', $text);
+        $out = null;
 
-<div class="fb-facepile" data-href="https://www.facebook.com/pages/The-Gaylord-Restaurant/219983038085736?rf=127973883918936" data-max-rows="5" data-colorscheme="light" data-size="large" data-show-count="true"></div>
 
-</body>
-</html>
-SELECT
-    o_id,
-    SUM(IF(o_paymentType= 'Cash', o_total, 0)) AS 'Cash',
-    SUM(IF(o_paymentType= 'Card', o_total, 0)) AS 'Card',
+        foreach ($text_words as $word) {
+            if ((strlen($word) > $len) && $out == null) {
 
-    SUM(o_total) AS SumTotal
-FROM
-    order_tbl
-WHERE
-    o_date= '2014-03-09'
-GROUP BY
-    o_id
+                return substr($word, 0, $len) . "...";
+            }
+            if ((strlen($out) + strlen($word)) > $len) {
+                return $out . "...";
+            }
+            $out.=" " . $word;
+        }
+        return $out;
+    }
+
+}
+
+?>
